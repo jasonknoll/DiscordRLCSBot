2b1730e72df096e6ec2b9e004c9fb8ca07afd345
Jason Knoll <jasonknoll98@aol.com>
2022-10-23 12:34:30 -0400

    Pls discord

---
+++ DiscordRLCSBot/Bot.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using Discord.Commands;
+
+public class Bot
+{
+    public static Task Main(String[] args) => new Bot().MainAsync();
+
+    private DiscordSocketClient _client;
+    private string URL_TO_AUTH = "auth.json";
+
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+
+        var text = File.ReadAllText(URL_TO_AUTH);
+        var tokenDict = JsonConvert.DeserializeObject<Dictionary<string,string>>(text);
+        
+        string token = tokenDict["token"].ToString();
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(Timeout.Infinite);
+    }
+
+    /**
+    * Simple logging message
+    */
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
+
--- ideas.md
+++ ideas.md
@@ -3,8 +3,13 @@
 ## Language
 ---
 * Python -> Fun to use, currently knowledgeable
-* C# -> Better than java imo, but not very good at it. Useful to make personal projects to benefit my skills at work.
+* C# -> Better than java imo, but not very good at it. Useful in make personal projects to benefit my skills at work.
 * Java -> no
+
+## API
+---
+https://zsr.octane.gg
+
 
 ## Features
 ---
@@ -19,4 +24,13 @@
 * Advanced Ideas
   * Generate visualizations from commands
   * cache data to prevent too many api calls
-  * 
+  * OctaneGG .NET API wrapper
+
+## Beta
+---
+I want to actually work on and complete this project so each 
+feature must go through some form of *beta* stage. I need to test
+everything completely to my highest standards (realistically) so
+that my bot isn't broken all the time. 
+
+Once tested I can move onto another feature. 
