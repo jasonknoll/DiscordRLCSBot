636618d53fc87236a9ba60c83d15ecd7051dfce2
Jason Knoll <jasonknoll98@aol.com>
2022-10-24 10:47:45 -0400

    Switch to DSharpPlus

--- DiscordRLCSBot/Bot.cs
+++ DiscordRLCSBot/Bot.cs
@@ -1,48 +1,58 @@
 // This uses Discord.NET
 // I want to switch to DSharpPlus
 
+/*
 using Discord;
 using Discord.WebSocket;
 using Newtonsoft.Json;
 using Discord.Commands;
+*/
+
+using System;
+using System.Threading.Tasks;
+using DSharpPlus; // TODO still need to install from NuGet!
+
 
 public class Bot
 {
-    public static Task Main(String[] args) => new Bot().MainAsync();
+    public static Task Main(String[] args) => new Bot().MainAsync().GetAwaiter().GetResult;
 
     // client object
-    private DiscordSocketClient _client;
+    private DiscordClient _client;
     
     // grab auth file from directory
     private string URL_TO_AUTH = "auth.json";
 
     public async Task MainAsync()
     {
-        _client = new DiscordSocketClient();
-
-        _client.Log += Log;
+        
 
         // grab credentials
         var text = File.ReadAllText(URL_TO_AUTH);
         var tokenDict = JsonConvert.DeserializeObject<Dictionary<string,string>>(text);
         
-        string token = tokenDict["token"].ToString();
+        // create new client with auth token
+        _client = new DiscordClient(
+            {
+                Token = tokenDict["token"].ToString();
+                TokenType = TokenType.Bot 
+            });
+        
+        // uses lambda method to handle event
+        // Docs recommend creating handlers for each event
+        // TODO - need to change this structure to handle diff modules
+        _client.MessageCreated += async (s, e) =>
+        {
+            if (e.Message.Content.ToLower().StartsWith("ping")
+                await e.Message.ResponseAsync("pong!");
+        }
 
         // bot login
-        await _client.LoginAsync(TokenType.Bot, token);
-        await _client.StartAsync();
+        await _client.ConnectAsync();
 
         // wait
-        await Task.Delay(Timeout.Infinite);
+        await Task.Delay(-1);
     }
 
-    /**
-    * Simple logging message -- following Discord.NET tutorial
-    */
-    private Task Log(LogMessage msg)
-    {
-        Console.WriteLine(msg.ToString());
-        return Task.CompletedTask;
-    }
 }
 
